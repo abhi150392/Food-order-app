8b08db2d0cae485456b22b7c13dc636994c1045e
Abhishek Vijayakumar <Abhishekwhizzer@gmail.com>
2023-02-03 01:34:55 -0500

    Added Functional components

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const header1 = React.createElement(
-  "h1",
-  {
-    id: "title1",
-  },
-  "Heading 1"
-);
+//React Element
+const Title = <h1>Title</h1>;
 
-const header2 = React.createElement(
-  "h2",
-  {
-    id: "title2",
-  },
-  "Heading 2"
-);
-const header3 = React.createElement(
-  "h3",
-  {
-    id: "title2",
-  },
-  "Heading 2"
-);
+//Functional Component
+const SubTitle = () => {
+  return <h2>Subtitle</h2>;
+};
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [header1, header2]
-);
+const Header = () => {
+  return (
+    <div>
+      {Title}
+      <SubTitle />
+      <h2>Im a Header</h2>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<Header />);
