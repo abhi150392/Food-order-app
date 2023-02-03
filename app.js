013270e70dca21b8bd8a9a134ee59ded4217852b
Abhishek Vijayakumar <Abhishekwhizzer@gmail.com>
2023-02-03 01:54:46 -0500

    added logo and search bar

--- app.js
+++ app.js
@@ -5,19 +5,25 @@ import ReactDOM from "react-dom/client";
 const Title = <h1>Title</h1>;
 
 //Functional Component
-const SubTitle = () => {
-  return <h2>Subtitle</h2>;
-};
-
-const Header = () => {
+const Logo = () => {
   return (
     <div>
-      {Title}
-      <SubTitle />
-      <h2>Im a Header</h2>
+      <img
+        src="https://marketplace.canva.com/EAFMNm9ybqQ/1/0/1600w/canva-gold-luxury-initial-circle-logo-qRQJCijq_Jw.jpg"
+        height="75px"
+      />
+    </div>
+  );
+};
+
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Logo />
+      <input type="text" placeholder="Search.." />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(<HeaderComponent />);
