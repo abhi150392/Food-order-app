bd9b3e4e386ad84aefcd7ef6d9df90f9dcc1f930
Abhishek Vijayakumar <Abhishekwhizzer@gmail.com>
2023-02-08 00:33:45 -0500

    added search functionality

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,49 @@
+import { useState } from "react";
 import { restrauntList } from "../constants";
 import RestrauntCard from "./RestrauntCard";
 
+const filteredData = (searchText, restaurants) => {
+  return restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+};
+
 const Body = () => {
+  /* Local variable
+  const searchTxt = "DOSA";
+  */
+  //React Variable or state Variable
+  const [restaurants, setRestaurants] = useState(restrauntList);
+  const [searchText, setSearchText] = useState("");
   return (
-    <div className="restraunt-list">
-      {restrauntList.map((restraunt) => {
-        return <RestrauntCard {...restraunt.data} key={restraunt.data.id} />;
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search for restaurants and food"
+          className="search-input"
+          value={searchText}
+          onChange={(e) => {
+            // console.log(e.target.value);
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filteredData(searchText, restaurants);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restraunt-list">
+        {restaurants.map((restraunt) => {
+          return <RestrauntCard {...restraunt.data} key={restraunt.data.id} />;
+        })}
+      </div>
+    </>
   );
 };
 
